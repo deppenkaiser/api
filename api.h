b33a263db480530681d6144cc6bd9a22572ab0c6
Dipl.-Ing. (FH) Michael Czybor <Michael.Czybor@Live.de>
2024-06-16 15:17:45 +0200

    REF new declaration type

--- api.h
+++ api.h
@@ -3,3 +3,5 @@
 #define private static
 #define protected
 #define protected_import(type, function) extern type function
+#define callback
+#define callback_declaration(type, function) extern type function __attribute__((weak))
