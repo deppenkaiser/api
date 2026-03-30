935b76e4edb95073a5b561af2552a55991b8cb07
Dipl.-Ing. (FH) Michael Czybor <Michael.Czybor@Live.de>
2026-03-30 11:57:10 +0200

    ...

--- api/api.h
+++ api/api.h
@@ -1,7 +1,9 @@
 #pragma once
 
-#define private static
-#define protected
-#define protected_import(type, function) extern type function
-#define callback
-#define callback_declaration(type, function) extern type function __attribute__((weak))
+#define private static																	// private in this.c
+
+#define protected																		// protected in this.lib definition (*.c)
+#define protected_import(type, function) extern type function							// protected in this.lib declaration (*.h)
+
+#define callback																		// virtual callback in application
+#define callback_declaration(type, function) extern type function __attribute__((weak))	// virtual callback declaration in this.lib (*.h)
