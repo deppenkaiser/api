30a32de638e6acbf85f4e8263969e9de31c3db83
Dipl.-Ing. (FH) Michael Czybor <Michael.Czybor@Live.de>
2026-09-25 11:14:26 +0200

    ...

--- api/api.h
+++ api/api.h
@@ -8,7 +8,7 @@
 #define callback																		// virtual callback in application
 #define callback_declaration(type, function) extern type function __attribute__((weak))	// virtual callback declaration in this.lib (*.h)
 
-#define PRIVATE_FUNC(type, function)        static type _##function
+#define PRIVATE_FUNC(type, function) static type _##function
 
 typedef bool* const bool_stack_t, *bool_heap_t;
 typedef int16_t* const int16_stack_t, *int16_heap_t;
@@ -19,3 +19,11 @@ typedef float* const float_stack_t, *float_heap_t;
 typedef double* const double_stack_t, *double_heap_t;
 
 // Aus einem x_heap_t Pointer kann nach NULL-Prüfung ein x_stack_t werden!
+
+#define CLASS(class_name) typedef struct class_##class_name* class_##class_name##_heap_t;\
+					struct class_##class_name
+
+#define CLASS_METHOD_PTR_DECL(class_name, type, function, arguments)	typedef type (*class_##class_name##_##function##_stack_t)(arguments);\
+																		PRIVATE_FUNC(type, function(arguments))
+
+#define CLASS_METHOD(class_name, type, function) type class_##class_name##_##function
